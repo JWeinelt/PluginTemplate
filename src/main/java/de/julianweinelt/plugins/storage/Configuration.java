08e1fd05631051613c1b7f0e9fab8bf10abf921d
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-21 08:55:41 +0200

    api: add configuration template class

--- src/main/java/de/julianweinelt/plugins/storage/Configuration.java
+++ src/main/java/de/julianweinelt/plugins/storage/Configuration.java
@@ -1,4 +1,38 @@
 package de.julianweinelt.plugins.storage;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Configuration {
+    /*
+    Add any configuration value you want to this class by declaring variables.
+    Please note that you should only store serializable values.
+    These are (but not limited to:)
+    - String
+    - primitives
+    - Wrapper classes
+
+    You should avoid using these types (there may be more):
+    - Color
+    - World
+    - Location
+    - Thread
+    - BufferedReader / BufferedWriter
+    ...
+
+    ProTip: If you declare a variable as transient, GSON won't try to serialize it!
+     */
+
+    // You don't need to declare any get/set methods.
+    // It's recommended to initialize them with placeholder values, otherwise they won't be saved as fields
+    // if no value is assigned.
+
+    // Here's an example for the database:
+    private String databaseHost = "";
+    private int databasePort = 3306;
+    private String databaseName = "";
+    private String databaseUser = "";
+    private String databasePassword = "";
 }
