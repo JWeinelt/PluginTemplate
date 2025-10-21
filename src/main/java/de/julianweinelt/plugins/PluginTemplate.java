3c3c4193bd08f8d05399e28366997d431af4998f
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-21 08:56:01 +0200

    api: add main template class

--- src/main/java/de/julianweinelt/plugins/PluginTemplate.java
+++ src/main/java/de/julianweinelt/plugins/PluginTemplate.java
@@ -1,17 +1,38 @@
 package de.julianweinelt.plugins;
 
+import de.julianweinelt.plugins.storage.LocalStorage;
+import de.julianweinelt.plugins.storage.SQLManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class PluginTemplate extends JavaPlugin {
+    @Getter
+    private static PluginTemplate instance;
+    @Getter
+    private static Logger log;
+
+    @Getter
+    private SQLManager sqlManager;
+    @Getter
+    private LocalStorage localStorage;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        log = getLogger();
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        localStorage = new LocalStorage(this);
+        localStorage.load();
+        sqlManager = new SQLManager(log, localStorage.getConfig());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (sqlManager != null) sqlManager.disconnect();
     }
 }
