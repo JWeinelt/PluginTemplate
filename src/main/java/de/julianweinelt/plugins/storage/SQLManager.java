13b6be93f960fd0e8cd4ed538e4d561f1205d1fc
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-21 08:56:09 +0200

    api: add database template class

--- src/main/java/de/julianweinelt/plugins/storage/SQLManager.java
+++ src/main/java/de/julianweinelt/plugins/storage/SQLManager.java
@@ -1,4 +1,18 @@
 package de.julianweinelt.plugins.storage;
 
-public class SQLManager {
+import de.codeblocksmc.codelib.databsae.MySQLTemplate;
+
+import java.util.logging.Logger;
+
+public class SQLManager extends MySQLTemplate {
+
+    public SQLManager(Logger log, Configuration c) {
+        super(log, c.getDatabaseHost(), c.getDatabasePort(), c.getDatabaseName(),
+                c.getDatabaseUser(), c.getDatabasePassword());
+    }
+
+    @Override
+    public void afterSuccessfulConnection() {
+        // Do something here
+    }
 }
